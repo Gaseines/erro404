2a1bf3bd06dbba0b83b9353e0860962c6e6a9712
Gaseines <gaseinesdzn@hotmail.com>
2026-02-11 15:52:17 -0300

    dsad

--- error/src/App.jsx
+++ error/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
+
 import Pagina from "./components/pagina";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <>
-      <div className="all">
-        <Pagina />
-      </div>
-    </>
+    <div className="all">
+      <Pagina />
+    </div>
   );
 }
 
--- error/src/components/Pagina.jsx
+++ error/src/components/Pagina.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 
 import styles from "./Pagina.module.css"
 
+import video from "../videos/aranha.mp4"
+
 export default function Pagina() {
   return (
-    <div>
-      <div className={styles.cabecalho}>
-        <p>logo.</p>
-        <li>
-            
-        </li>
+    <>
+    <div className={styles.cabecalho}>
+        <p className={styles.logo}>logo.</p>
+        <ul className={styles.lista}>
+          <li>Início</li>
+          <li>Sobre nós</li>
+          <li>Blog</li>
+          <li>Contato</li>
+        </ul>
+        <p className={styles.login}>Login</p>
+      </div>
+    <div className={styles.pagina}>
+      
+      <div className={styles.err_back}>
+        <video className={styles.video} src={video} muted autoPlay></video>
+        <div className={styles.err}>
+          <p>4</p>
+          <p>4</p>
+        </div>
       </div>
     </div>
+    </>
   )
 }
